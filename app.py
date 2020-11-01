22574c0aeafdf23d69c7c800d39c14a7c3aeaf31
Silvia Metelli <silvia.metelli1@gmail.com>
2020-11-01 18:36:00 +0100

    Added tabs and data loader

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-import os
+import os, io
 import pandas as pd, numpy as np
 import dash, dash_core_components as dcc, dash_html_components as html
+import dash_table
+
+
 import dash_cytoscape as cyto
 
 from dash.exceptions import PreventUpdate
@@ -37,11 +40,10 @@ network_layouts = [{'label': 'circle',       'value': 'circle'},
                    {'label': 'breadthfirst', 'value': 'breadthfirst'},
                    {'label': 'cose',         'value': 'cose'}]
 
+DF = pd.read_csv('db/Senn2013.csv', index_col=0).rename(columns={"treat1.long": 'from',
+                                                                 "treat2.long": 'to'})
 def get_network():
-    df = pd.read_csv('db/Senn2013.csv', index_col=0).rename(columns={"treat1.long": 'from',
-                                                                     "treat2.long": 'to',
-                                                                     'TE': 'weight'})
-    edges = df.groupby(['from', 'to']).weight.count().reset_index()
+    edges = DF.groupby(['from', 'to']).TE.count().reset_index()
     cy_edges = [{'data': {'source': source, 'target': target, 'weight': weight * 2, 'weight_lab': weight}}
                 for source, target, weight in edges.values]
     cy_nodes = [{"data": {"id": target, "label": target}}
@@ -61,7 +63,7 @@ app.layout = html.Div(
                       className="app__header__logo")],
              className="app__header"),
     html.Div([html.Div(   # NMA Graph
-                 [html.Div([html.H6("NMA Graph", className="graph__title"),
+                 [html.Div([html.H6("NMA Graph", className="graph__title",style={'display': 'inline-block'}),
                             html.Div([dcc.Dropdown(id='dropdown-layout', options=network_layouts, clearable=False,
                                                    value='circle', style={'width':'170px',
                                                                           'color': '#1b242b',
@@ -78,36 +80,55 @@ app.layout = html.Div(
                             html.Div([html.P(id='cytoscape-mouseoverEdgeData-output', className="info_box"),
                                       html.Br()],
                                       className="content_information"),
-                            html.Div([],
-                                      className="auto__container")],
-                          className="graph__container first"),
+                            html.Div([],className="auto__container")],
+                          className="info__container"),
                           # Forest Plot
-                          html.Div([html.Div([html.H6(id='tapNodeData-info', className="box__title"), html.Br()]),
-                                dcc.Loading(
-                                    dcc.Graph(id='tapNodeData-fig',
-                                              config={'modeBarButtonsToRemove':['toggleSpikelines', "pan2d",
-                                                                                "select2d", "lasso2d", "autoScale2d",
-                                                                                "hoverCompareCartesian"],
-                                                      'toImageButtonOptions': {'format': 'png', # one of png, svg,
-                                                                               'filename': 'custom_image',
-                                                                               'scale': 10 # Multiply title/legend/axis/canvas sizes by this factor
-                                                                              },
-                                                      'displaylogo':False}
-                                    ))],
-                                   className="graph__container second")],
-                      className="one-half column")],
+                          html.Div([dcc.Tabs([
+                              dcc.Tab(label='Forest plot',
+                                      children=[html.Div([html.P(id='tapNodeData-info', className="box__title"),
+                                                          html.Br()]),
+                                                dcc.Loading(
+                                                    dcc.Graph(id='tapNodeData-fig',
+                                                              config={'modeBarButtonsToRemove':['toggleSpikelines', "pan2d",
+                                                                                                "select2d", "lasso2d", "autoScale2d",
+                                                                                                "hoverCompareCartesian"],
+                                                                      'toImageButtonOptions': {'format': 'png', # one of png, svg,
+                                                                                               'filename': 'custom_image',
+                                                                                               'scale': 10 # Multiply title/legend/axis/canvas sizes by this factor
+                                                                                              },
+                                                                      'displaylogo':False}))]
+                                      ),
+                              dcc.Tab(label='Data', children=[dcc.Upload(html.Button('Upload your file!',
+                                                                                     style=dict(color='white')),
+                                                                         id='datatable-upload'),
+                                                              dash_table.DataTable(id='datatable-upload-container',
+                                                                                   fixed_rows={'headers': True,
+                                                                                               'data': 0},
+                                                                                   style_cell_conditional=[
+                                                                                       {'if': {'column_id': c},
+                                                                                               'textAlign': 'left'}
+                                                                                       for c in ['Date', 'Region']],
+                                                                                   style_data_conditional=[
+                                                                                       {'if': {'row_index': 'odd'},
+                                                                                               'backgroundColor': 'rgb(248, 248, 248)'}],
+                                                                                   style_header={'backgroundColor': 'rgb(230, 230, 230)',
+                                                                                                 'fontWeight': 'bold'},
+                                                                                   css=[{"selector": "table",
+                                                                                             "rule": "width: 100%;"}]
+                                                                                   )])
+                          ],colors={"border": "#1b242b",
+                                    "primary": "#1b242b",
+                                    "background": "#1b242b"}, style=dict(color='#40515e', fontWeight= 'bold')),
+                          ],
+                                   className="graph__container second"),
+
+                      ],
+                      className="one-half column"),
+
+
+    ],
               className="app__content")],
     className="app__container")
-
-
-
-
-# @app.callback(Output('forest_plot', 'layout'),
-#               [Input('dropdown-layout', 'value')])
-# def forest_plot():
-#     df = px.data.iris()
-#     fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species", title="A Plotly Express Figure")
-#     return fig
 
 
 
@@ -124,9 +145,9 @@ def update_cytoscape_layout(layout):
               [Input('cytoscape', 'tapNodeData')])
 def TapNodeData_info(data):
     if data:
-        return 'Forest plot: ' + data['label']
+        return data['label']
     else:
-        return 'Forest plot (click on a node to display associated forest plot)'
+        return 'Click on a node to display associated forest plot'
 
 @app.callback(Output('tapNodeData-fig', 'figure'),
               [Input('cytoscape', 'tapNodeData')])
@@ -196,10 +217,41 @@ def TapNodeData_fig(data):
 def mouseoverEdgeData(data):
     if data:
         n_studies = data['weight_lab']
-        studies_str = f"{n_studies}" + (' studies' if n_studies>1 else 'study')
+        studies_str = f"{n_studies}" + (' studies' if n_studies>1 else ' study')
         return f"{data['source'].upper()} vs {data['target'].upper()}: {studies_str}"
     else:
         return "Try hovering over an edge!"
+
+# @app.callback(Output('datatable-upload-container', 'children'),
+#               [Input('upload', 'contents')])
+# def update_figure(content):
+#     if not content:
+#         return []
+#     dff = pd.read_csv(io.StringIO(content))
+#     return dff.to_dict('records')
+import base64
+def parse_contents(contents, filename):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    if 'csv' in filename:
+        # Assume that the user uploaded a CSV file
+        return pd.read_csv(
+            io.StringIO(decoded.decode('utf-8')))
+    elif 'xls' in filename:
+        # Assume that the user uploaded an excel file
+        return pd.read_excel(io.BytesIO(decoded))
+
+
+@app.callback([Output('datatable-upload-container', 'data'),
+               Output('datatable-upload-container', 'columns')],
+              [Input('datatable-upload', 'contents')],
+              [State('datatable-upload', 'filename')])
+def update_output(contents, filename):
+    if contents is None:
+        df = DF
+    else:
+        df = parse_contents(contents, filename)
+    return df.to_dict('records'), [{"name": i, "id": i} for i in df.columns]
 
 
 

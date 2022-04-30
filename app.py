0b43fedf262c211244b46cae8c6c6c02a482de62
Silvia Metelli <silvia.metelli1@gmail.com>
2022-05-01 01:03:18 +0200

    cinema coloring upon selection fixed

--- app.py
+++ app.py
@@ -1356,7 +1356,7 @@ if __name__ == '__main__':
     # Talisman(app.server, content_security_policy=None)
     # context = generate_ssl_perm_and_key(cert_name='cert.pem', key_name='key.pem')
     # app.run_server(debug=False, ssl_context=context)
-    app.run_server(debug=True)
+    app.run_server(debug=False)
 
 
 

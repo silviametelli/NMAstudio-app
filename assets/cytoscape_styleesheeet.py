a9bad89f10de25044cf5236c01456c776ed40b0b
Silvia Metelli <silvia.metelli1@gmail.com>
2022-05-25 11:47:33 +0200

    cinema filtering one outcome minor fixing

--- assets/cytoscape_styleesheeet.py
+++ assets/cytoscape_styleesheeet.py
@@ -2,7 +2,7 @@ from assets.COLORS import *
 from assets.storage import N_CLASSES
 from tools.utils import CMAP
 
-def get_stylesheet(node_size=False, classes=False, n_class=N_CLASSES, edg_col=DFLT_EDG_CLR, nd_col=DFLT_ND_CLR, edge_size=False,
+def get_stylesheet(node_size=False, classes=False, n_class=N_CLASSES, edg_col=False, nd_col=DFLT_ND_CLR, edge_size=False,
                    pie=False, edg_lbl=False, nodes_opacity=1, edges_opacity=0.77):
     cmaps_class = CMAP[ :n_class] if n_class > 1 else [DFLT_ND_CLR]
     default_stylesheet = [
@@ -19,7 +19,7 @@ def get_stylesheet(node_size=False, classes=False, n_class=N_CLASSES, edg_col=DF
         {"selector": 'edge',
          'style': {"curve-style": "bezier",
                    'width': 'data(weight)',
-                   'line-color': edg_col,
+                #   'line-color': edg_col,
                     "opacity": edges_opacity}}]
     if classes:
        # default_stylesheet[0]['style'].update({"shape": "triangle"})
@@ -30,6 +30,7 @@ def get_stylesheet(node_size=False, classes=False, n_class=N_CLASSES, edg_col=DF
            default_stylesheet.append(x)
     if node_size: default_stylesheet[0]['style'].update({"width": "data(size)", "height": "data(size)"})
     if edge_size: default_stylesheet[1]['style'].update({"width": None})
+#    if edg_col:   default_stylesheet[1]['style'].update({'line-color': edg_col})
     if edg_lbl:   default_stylesheet[1]['style'].update({'label': 'data(weight_lab)'})
     if pie:       default_stylesheet[0]['style'].update({
                                                'pie-1-background-color': '#E8747C',

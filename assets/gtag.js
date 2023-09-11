c64fdf7a35ff353f40d573110121bc56edb765f6
Tianqi Yu <leatqy@outlook.com>
2023-09-11 10:55:39 +0200

    add google analytics

---
+++ assets/gtag.js
@@ -0,0 +1,8 @@
+
+src="https://www.googletagmanager.com/gtag/js?id=G-Y7P5T0R3ML"
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Y7P5T0R3ML');
